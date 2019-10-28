5f23dd42bd400c9e685322293a53787993611618
TLuigi003 <tluigi003.l3@gmail.com>
2019-10-28 21:30:13 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.downloadFile = (req, res) => {
+    const url = req.body.url;
+    const fileName = req.body.fileName;
+    const child_process = require('child_process');
+
+    var wget = 'wget -P /tmp/ -O' + fileName + ' ' + url;
+  // excute wget using child_process' exec function
+
+  child_process.execSync(wget);
+    // Imports the Google Cloud client library
+    const { Storage } = require('@google-cloud/storage');
+
+    // Creates a client
+    const storage = new Storage();
+    const bucketName = 'luismayo-audio-input';
+    const filename = '/tmp/' + fileName;
+
+    // Uploads a local file to the bucket
+    storage.bucket(bucketName).upload(filename, {
+        gzip: true,
+        metadata: {
+            cacheControl: 'no-cache',
+        },
+    });
+
+    res.status(200).send(`${filename} uploaded to ${bucketName}.`);
+
+};
